1454b748950756b16666bdd5a177b4d479b28b67
root <root@simplescontrole01-lamp-nyc3-01.localdomain>
2017-08-21 10:27:06 -0300

    local changes

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=superapi',
-    'username' => 'root',
-    'password' => '123',
+    'dsn' => 'mysql:host=localhost;dbname=axs_superapi',
+    'username' => 'brwsapi',
+    'password' => 'vNC9@-ask7IlR8)ks#$-92PI%9ghx',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
